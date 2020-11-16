5385548b597e9e2d30b20754074ba567e701c9ae
synthroot <synthroot@gmail.com>
2020-11-16 09:01:39 -0500

    first

---
+++ MovieRegistration/Controllers/Registration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class Registration : Controller
+    {
+        public IActionResult Register()
+        {
+            return View(new Movie());
+        }
+        public IActionResult Result(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(movie);
+            }
+            else
+            {
+                return View(nameof(Register), movie);
+            }
+        }
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public int Year { get; set; }
+
+        public List<string> Actors { get; set; } = new List<string>();
+
+        public List<string> Directors { get; set; } = new List<string>();
+    }
+}
